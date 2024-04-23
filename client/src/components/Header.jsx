76286329d577ddd68024c686ca0bc9484a066790
kal_olani <123155716+kalolani@users.noreply.github.com>
2024-04-24 01:33:54 +0300

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch("http://localhost:4000/profile", {
+    fetch("https://blog-website-api-murex.vercel.app/profile", {
       credentials: "include",
     }).then((response) => {
       response.json().then((userInfo) => {
@@ -30,7 +30,7 @@ function Header() {
   console.log(emailInfo);
 
   function logout() {
-    fetch("http://localhost:4000/logout", {
+    fetch("https://blog-website-api-murex.vercel.app/logout", {
       method: "POST",
       credentials: "include",
     });
