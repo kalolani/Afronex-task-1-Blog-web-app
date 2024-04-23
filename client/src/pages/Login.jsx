dfb85a4c7a00a8f05391883ce301ec740175629b
kal_olani <123155716+kalolani@users.noreply.github.com>
2024-04-24 00:57:03 +0300

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
 
   async function login(e) {
     e.preventDefault();
-    const response = await fetch("http://localhost:4000/login", {
+    const response = await fetch("https://blog-website-api-murex.vercel.app/login", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
