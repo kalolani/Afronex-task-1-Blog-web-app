082dac0b94f7281d03044acade1affa4521f85dc
kal_olani <123155716+kalolani@users.noreply.github.com>
2024-04-24 01:51:11 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -228,4 +228,7 @@ app.use((err, req, res, next) => {
   }
 });
 
-app.listen(4000);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
