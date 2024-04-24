e650cf2d911dc22d553ba220104b5b4fc6060970
kal_olani <123155716+kalolani@users.noreply.github.com>
2024-04-24 08:14:04 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,18 +14,12 @@ import Comment from "./models/comment.js";
 
 const app = express();
 
-const allowedOrigins = ['https://blog-website-frontend-opal.vercel.app'];
+const allowedOrigin = 'https://blog-website-frontend-opal.vercel.app';
 app.use(cors({
-  origin: function(origin, callback) {
-    // Check if the request origin is allowed
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods:["POST","GET","PUT"],
+  origin: allowedOrigin,
+  methods:["POST","GET","PUT" ],
   credentials:true
+  
 }));
 app.use(express.json());
 app.use(cookieParser());
