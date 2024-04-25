b4c4af2ccba739e2cc1c4dcf0bb0f20028a740cc
kal_olani <123155716+kalolani@users.noreply.github.com>
2024-04-25 13:45:22 +0300

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,24 +18,15 @@ function Header() {
   const { emailInfo, setEmailInfo, isOpen, setIsOpen } = usePosts();
   const navigate = useNavigate();
 
-  useEffect(() => {
+ useEffect(() => {
     fetch("https://blog-website-api-murex.vercel.app/profile", {
       credentials: "include",
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch");
-        }
-        return response.json();
-      })
-      .then((userInfo) => {
+    }).then((response) => {
+      response.json().then((userInfo) => {
         setEmailInfo(userInfo);
-      })
-      .catch((error) => {
-        console.error("Error fetching profile:", error);
-        // Handle error, e.g., set default values or show error message
       });
-  }, []);
+    });
+  }, [emailInfo]);
 
   console.log(emailInfo);
 
